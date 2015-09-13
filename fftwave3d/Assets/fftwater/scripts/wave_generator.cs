7afc866466cd92d2c25cdd5ab877209d85abd658
zombiecry <yangsc2009@163.com>
2015-09-13 19:00:41 +0800

    add fft for wave solver

--- fftwave3d/Assets/fftwater/scripts/wave_generator.cs
+++ fftwave3d/Assets/fftwater/scripts/wave_generator.cs
@@ -1,5 +1,5 @@
-﻿//#define NO_FT
-
+﻿#define NO_FT
+//#define USE_FFT
 using UnityEngine;
 using System.Collections;
 [ExecuteInEditMode]
@@ -17,6 +17,8 @@ public class wave_generator : MonoBehaviour {
 
 	private int real_z = 8;
 	private int real_x = 8;
+
+
 	private float grid_step_x = 0;
 	private float grid_step_z = 0;
 	private Vector3 [] displace_map;
@@ -41,6 +43,9 @@ public class wave_generator : MonoBehaviour {
 		public static Complex operator +(Complex lhs, Complex rhs){
 			return new Complex (lhs.real_ + rhs.real_, lhs.image_ + rhs.image_);
 		}
+		public static Complex operator -(Complex lhs, Complex rhs){
+			return new Complex (lhs.real_ - rhs.real_, lhs.image_ - rhs.image_);
+		}
 		public override string ToString(){
 			return "("+real_.ToString()+","+image_.ToString()+")";
 		}
@@ -51,6 +56,7 @@ public class wave_generator : MonoBehaviour {
 	//fft related               
 	private Complex [] h_tilde;
 	// Use this for initialization
+
 	void Start () {
 		//init
 		displace_map = new Vector3[res_x * res_z];
@@ -143,7 +149,6 @@ public class wave_generator : MonoBehaviour {
 	}
 	// h0(k)*exp(i*wk*t)
 	//+h0(-k).conj()*exp(-i*wk*t)
-
 	void dft(Complex [] data_in,Complex [] data_out,int start,int stride,int N){
 		Complex [] temp_res = new Complex[N];
 		for (int k=0; k<N; k++) {
@@ -156,10 +161,87 @@ public class wave_generator : MonoBehaviour {
 			data_out[start+k*stride]=temp_res[k];
 		}
 	}
+	private int fast_log_2(int x){
+		int count = 0;
+		while ((x/=2)>0)	count++;
+		return count;
+	}
+	private int flip(int x,int n){
+		int l = 1;
+		int h = n;
+		int y = x;
+		while (l<h) {
+			int bh = x&h;
+			int bl = x&l;
+			if ((bh>0) != (bl>0)){
+				y&=~h;
+				y&=~l;
+				y|=(~bh)&h;
+				y|=(~bl)&l;
+			}
+			h=h>>1;
+			l=l<<1;
+		}
+		return y;
+	}
+	static void Swap(ref Complex lhs, ref Complex rhs)
+	{
+		Complex temp;
+		temp = lhs;
+		lhs = rhs;
+		rhs = temp;
+	}
+
+	private void reorder(Complex [] data,int start,int stride,int N){
+		//string temp = "";
+		for (int i=0; i<N; i++) { 
+			int j = flip(i,N>>1);
+			if (i<j) {
+				/*
+				Debug.Log("--------------------------------");
+				Debug.Log(data[start+i*stride].ToString());
+				Debug.Log(data[start+j*stride].ToString());
+
+				Debug.Log(data[start+i*stride].ToString());
+				Debug.Log(data[start+j*stride].ToString());
+				Debug.Log("--------------------------------");
+				*/
+				/*
+				temp +="<";
+				temp +=i.ToString();
+				temp +=":";
+				temp+=j.ToString();
+				temp+="> ";
+				*/
+				Swap(ref data[start+i*stride],ref data[start+j*stride]);
+			}
+		}
+		//Debug.Log (temp);
+	}
 	//dft
-	void fft(Complex [] data_in,Complex [] data_out,int start,int stride){
-		for (int i=start; i<data_in.Length; i+=stride) {
-			data_out[i]=data_in[i];
+	void fft(Complex [] data_in,Complex [] data_out,int start,int stride,int N){
+		for (int i=0; i<N; i++) {
+			data_out[start+i*stride] = data_in[start+i*stride];
+		}
+		reorder (data_out,start,stride,N);
+		int log_n =fast_log_2(N);
+		for (int m=0;m<log_n;m++){					//stage
+			int nm=1<<(m+1);
+			Complex w = new Complex(1,0);
+			Complex wm = Expi(-2*Mathf.PI/(float)nm);
+			int freq_num = nm>>1;
+			for (int k=0;k<freq_num;k++){			//every freq in cur stage
+				for (int i=k;i<N;i+=nm){			//every butterfly in cur freq,start with offset of k
+					int p=i;
+					int q=i+freq_num;				//freq_num is Nm/2
+					int p_pos=start+p*stride;
+					int q_pos=start+q*stride;
+					Complex t = w*data_out[q_pos];
+					data_out[q_pos]=data_out[p_pos]-t;
+					data_out[p_pos]=data_out[p_pos]+t;
+				}
+				w=w*wm;
+			}
 		}
 	}
 	Complex calc_htilde(int m,int n,float t){
@@ -169,23 +251,17 @@ public class wave_generator : MonoBehaviour {
 	}
 
 	void gen_dis_map(float t){
-	//	var k = calc_k (4, 4);
-
-		//Debug.Log (PhillipsTerm (k).ToString ());
-		string temp="";
 		for (int m=0;m<res_x;m++) for (int n=0;n<res_z;n++){
 			h_tilde[m*res_z+n] = calc_htilde(m,n,t);
 			//temp +="<"+m.ToString()+","+n.ToString ()+">";
 			//temp +=h_tilde[m*res_z+n].ToString();
 			//temp+= calc_k (m, n).ToString();
-
 		}
 
 #if NO_FT
 		Complex [] temp1 = new Complex[res_x * res_z];
 		for (int x=0; x<res_x; x++) for (int z=0; z<res_z; z++) {
 				temp1 [x * res_z + z] = new Complex (0, 0);
-				//Vector2 vx = new Vector2 ((x-res_x/2.0f) * real_x/(float)res_x, (z-res_z/2.0f)*real_z/(float)res_z);
 				Vector2 vx = new Vector2((x) * real_x/(float)res_x, (z)*real_z/(float)res_z);
 				for (int m=0; m<res_x; m++) for (int n=0; n<res_z; n++) {
 						Vector2 k = calc_k (m, n);
@@ -198,10 +274,18 @@ public class wave_generator : MonoBehaviour {
 #else
 
 		for (int m=0;m<res_x;m++){
+#if USE_FFT
+			fft (h_tilde,h_tilde,m*res_z,1,res_z);
+#else
 			dft (h_tilde,h_tilde,m*res_z,1,res_z);
+#endif
 		}
 		for (int q=0; q<res_z; q++) {
+#if USE_FFT
+			fft (h_tilde,h_tilde,q,res_z,res_x);
+#else
 			dft (h_tilde,h_tilde,q,res_z,res_x);
+#endif
 		}
 		float sign = 1;
 		for (int i=0; i<res_x; i++) for (int j=0; j<res_z; j++) {
